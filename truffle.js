7c3daeba007fe7bb1b381a910ec0ede537823047
desmond <desmond@gramophone.co.in>
2019-07-07 21:40:55 +0530

    init

---
+++ truffle.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/xxx`),
+      network_id: '*',       // Ropsten's id
+      gas: 4500000,
+      gasPrice: 10000000000,        // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  },
+  compilers:{
+    solc:{
+      version:'^0.4.24'
+    }
+  }
+};
